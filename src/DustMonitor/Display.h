711dc0592b373cdb940757489ff29aad74058bab
dreamasza3 <48270092+dreamasza3@users.noreply.github.com>
2019-03-11 10:56:22 +0700

    a
    initail release

---
+++ src/DustMonitor/Display.h
@@ -0,0 +1,5 @@
+#ifndef Dis_play_H
+#define Dis_play_H
+void DisplaySetup();  // This is a task.
+void TaskDisplay(void *pvParameters);
+#endif
--- src/DustMonitor/DustMonitor.ino
+++ src/DustMonitor/DustMonitor.ino
@@ -1,15 +1,17 @@
 #include "Blink.h"
 #include "Network.h"
 #include "DustSensor.h"
-
+#include "Display.h"
 // the setup function runs once when you press reset or power the board
+ const int buttonPin = 0; 
+int buttonState = 0; 
 void setup() {
-  
+ pinMode(buttonPin, INPUT);
   // initialize serial communication at 115200 bits per second:
   Serial.begin(115200);
-
+  DisplaySetup();
   BlinkSetup();
-  NetWorkSetup();
+//  NetWorkSetup();
   DustSensorSetup();
   // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
 }
@@ -17,6 +19,13 @@ void setup() {
 
 void loop()
 {
-  Serial.print("aaaa");// Empty. Things are done in Tasks.
-  vTaskDelay(2000);
+//  Serial.print("aaaa");// Empty. Things are done in Tasks.
+//  vTaskDelay(2000);
+      buttonState = digitalRead(buttonPin);
+        if (buttonState == HIGH) {
+         void TaskDisplay(void *pvParameters);
+         }
+        else {
+      
+         }
 }
