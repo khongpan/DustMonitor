f3d5d36be68c6023ca1116b918c4d400cda973cf
khongpan <khongpan@gmail.com>
2019-03-21 22:39:46 +0700

    Update Blink.cpp

--- src/DustMonitor/Blink.cpp
+++ src/DustMonitor/Blink.cpp
@@ -1,8 +1,5 @@
 #include "Arduino.h"
 
-#include <Wire.h>    // I2C library
-#include "ccs811.h"  // CCS811 library
-
 #ifndef LED_BUILTIN
 //#define LED_BUILTIN 25
 #define LED_BUILTIN 16
@@ -13,61 +10,6 @@
 #else
 #define ARDUINO_RUNNING_CORE 1
 #endif
-
-
-static CCS811 ccs811(23);
-void SetupCCS811() {
-
-  Serial.println("");
-  Serial.println("setup: Starting CCS811 basic demo");
-  Serial.print("setup: ccs811 lib  version: "); Serial.println(CCS811_VERSION);
-
-  // Enable I2C
-  Wire.begin(); 
-  
-  // Enable CCS811
-  ccs811.set_i2cdelay(50); // Needed for ESP8266 because it doesn't handle I2C clock stretch correctly
-  bool ok= ccs811.begin();
-  if( !ok ) Serial.println("setup: CCS811 begin FAILED");
-
-  // Print CCS811 versions
-  Serial.print("setup: hardware    version: "); Serial.println(ccs811.hardware_version(),HEX);
-  Serial.print("setup: bootloader  version: "); Serial.println(ccs811.bootloader_version(),HEX);
-  Serial.print("setup: application version: "); Serial.println(ccs811.application_version(),HEX);
-  
-  // Start measuring
-  ok= ccs811.start(CCS811_MODE_1SEC);
-  if( !ok ) Serial.println("setup: CCS811 start FAILED");
-  
-}
-
-void Loop_CCS811() {
-
-
-  uint16_t eco2, etvoc, errstat, raw;
-  ccs811.read(&eco2,&etvoc,&errstat,&raw); 
-  
-  // Print measurement results based on status
-  if( errstat==CCS811_ERRSTAT_OK ) { 
-    Serial.print("CCS811: ");
-    Serial.print("eco2=");  Serial.print(eco2);     Serial.print(" ppm  ");
-    Serial.print("etvoc="); Serial.print(etvoc);    Serial.print(" ppb  ");
-    //Serial.print("raw6=");  Serial.print(raw/1024); Serial.print(" uA  "); 
-    //Serial.print("raw10="); Serial.print(raw%1024); Serial.print(" ADC  ");
-    //Serial.print("R="); Serial.print((1650*1000L/1023)*(raw%1024)/(raw/1024)); Serial.print(" ohm");
-    Serial.println();
-  } else if( errstat==CCS811_ERRSTAT_OK_NODATA ) {
-    Serial.println("CCS811: waiting for (new) data");
-  } else if( errstat & CCS811_ERRSTAT_I2CFAIL ) { 
-    Serial.println("CCS811: I2C error");
-  } else {
-    Serial.print("CCS811: errstat="); Serial.print(errstat,HEX); 
-    Serial.print("="); Serial.println( ccs811.errstat_str(errstat) ); 
-  }
-}
-
-
-
 
 
 /*--------------------------------------------------*/
@@ -88,7 +30,7 @@ void TaskBlink(void *pvParameters)  // This is a task.
 
   // initialize digital LED_BUILTIN on pin 13 as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-  //SetupCCS811();
+
 
   for (;;) // A Task shall never return or exit.
   {
@@ -96,7 +38,6 @@ void TaskBlink(void *pvParameters)  // This is a task.
     vTaskDelay(900);  // one tick delay (15ms) in between reads for stability
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
     vTaskDelay(100);  // one tick delay (15ms) in between reads for stability
-    //Loop_CCS811();
   }
 }
 
